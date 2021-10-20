3ca50f0113d8696c37018b6f45dfdd9447c5d8e9
gis212 <49344194+gis212@users.noreply.github.com>
2021-10-21 00:11:35 +0300

    Add files via upload
    hey eden! i uploaded the project with out the node modules and the public ofc

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+let { readFileSync } = require('fs')
+let { load } = require("js-yaml")
+let poststylus = () => require('poststylus')(['autoprefixer', 'postcss-font-magician', 'cssnano'])
+module.exports = function(grunt) { 
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    pug: {
+      templates: {
+        options: {
+          i18n: {
+            data: {$default_i18n: load(readFileSync('./locales/_default.yaml', 'utf8'))},
+            locales: 'locales/!(_default).yaml',
+            namespace: '$i18n',
+          },
+        },
+        files: {
+          'public/index.html': ['index.pug'],
+        },
+      },
+    },
+    stylus: {
+      compile: {
+        options: { use: [poststylus] },
+        files: { 'public/style.css': 'style.styl' },
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          { expand: true, src: ['img/**'], dest: 'public/' },
+          { expand: true, src: ['index.html'], dest: 'public/' },
+        ],
+      },
+    },
+  })
+  grunt.loadNpmTasks('grunt-pug-i18n')
+  grunt.loadNpmTasks('grunt-contrib-stylus')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.registerTask('default', ['pug', 'stylus', 'copy'])
+}
